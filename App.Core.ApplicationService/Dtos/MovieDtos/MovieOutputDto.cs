6b802e04a916882ac4f5823df51fbd8f449a0982
yousefnaddafi <yousefnaddafi@gmail.com>
2021-02-01 19:54:55 +0330

    ksdjfh

---
+++ App.Core.ApplicationService/Dtos/MovieDtos/MovieOutputDto.cs
@@ -0,0 +1,19 @@
+﻿using App.Core.Entities.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Core.ApplicationService.Dtos.MovieDtos
+{
+    public class MovieOutputDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Genre> Genres { get; set; }
+        public string Director { get; set; }
+        public string ProductorYear { get; set; }
+        public string Summery { get; set; }
+        public string ImdbRate { get; set; }
+        public float Rate { get; set; }
+    }
+}
