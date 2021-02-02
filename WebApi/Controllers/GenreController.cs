3ac921a87da44e72e97e134db70b019e81f1f230
yousefnaddafi <yousefnaddafi@gmail.com>
2021-02-02 17:37:41 +0330

    wef

---
+++ WebApi/Controllers/GenreController.cs
@@ -0,0 +1,45 @@
+﻿using App.Core.ApplicationService.ApplicationSerrvices.Genres;
+using App.Core.Entities.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly IGenreService GenreService;
+
+        public GenreController(IGenreService GenreService)
+        {
+            this.GenreService = GenreService;
+        }
+        [HttpPost]
+        public void Create(Genre inputDto)
+        {
+            GenreService.Create(inputDto);
+        }
+        [HttpPut]
+        public Genre Update(Genre item)
+        {
+            this.GenreService.Update(item);
+            return item;
+        }
+        [HttpDelete]
+        public int Delete(int id)
+        {
+            GenreService.Delete(id);
+            return id;
+        }
+        [HttpGet]
+        public Genre Get(int id)
+        {
+            return GenreService.Get(id);
+        }
+    }
+}
