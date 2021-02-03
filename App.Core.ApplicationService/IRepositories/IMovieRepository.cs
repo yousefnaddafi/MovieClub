7de09efcb506c5f369d85d9215a8829a5e85cd45
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-02-03 11:34:59 +0330

    a

--- App.Core.ApplicationService/IRepositories/IMovieRepository.cs
+++ App.Core.ApplicationService/IRepositories/IMovieRepository.cs
@@ -16,7 +16,7 @@ namespace App.Core.ApplicationService.IRepositories
         
         T Update(T item);
         void Delete(int id);
-        IQueryable<T> Query { get; }
+        
 
         void Save();
     }
