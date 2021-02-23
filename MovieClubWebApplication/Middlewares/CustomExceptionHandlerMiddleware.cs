0fb85716ee60241b269cbcb6a03f976a43f875b6
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-02-23 22:00:20 +0330

    aaaaa

---
+++ MovieClubWebApplication/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using App.Core.Entities.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MovieClubWebApplication.Middlewares
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case InvalidValuesException e:
+                        // custom application error
+                        response.StatusCode = (int)HttpStatusCode.NotAcceptable;
+                        break;
+                    case InvalidIdException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotAcceptable;
+                        break;
+
+                    case KeyNotFoundException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
