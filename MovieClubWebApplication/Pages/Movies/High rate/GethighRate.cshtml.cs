fc9a7e21ecd1a4d848b60a4d15059498a15d2647
forough_asgari <77478727+AStudent-art-maker2@users.noreply.github.com>
2021-03-04 02:44:14 +0330

    get high popular

---
+++ MovieClubWebApplication/Pages/Movies/High rate/GethighRate.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Core.ApplicationService.ApplicationSerrvices.Movies;
+using App.Core.ApplicationService.Dtos.MovieDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MovieClubWebApplication.Pages.Movies
+{
+    public class GethighRateModel : PageModel
+    {
+        private readonly IMovieService _movieService;
+        public GethighRateModel(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        [BindProperty]
+        public List<MovieOutputDto> movieInput { get; set; }
+        public async Task OnGetAsync()
+        {
+            movieInput = await _movieService.GetHighRate();
+
+        }
+    }
+}
